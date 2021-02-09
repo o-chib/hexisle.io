2f6ff26b46d9a5ba71025347f25ec2dfb3ce6dbd
Alex Dunn <adunn@ucdavis.edu>
2021-02-09 02:06:48 -0800

    create a file for constansts and player

---
+++ src/shared/constants.ts
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    // Player_Radius, Player_Hp, Player_Speed, Player_Fire_rate, Bullet_speed, Bullet damage
+    
+    message: {
+        JOIN: 'join'
+    }
+});
