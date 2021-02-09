1b01c41bad47d05269ced93f7ef3f15a0a2fa3e2
Oorjit Chib <ochib@ucdavis.edu>
2021-02-09 03:05:52 -0800

    Fn for drawing a column of tiles + cleanup
    - Removed all tileSize mentions in favor of private hexSize variable.
    - Comments and sandbox work

--- src/client/scenes/hexTileScene.ts
+++ src/client/scenes/hexTileScene.ts
@@ -1,6 +1,6 @@
 export default class HexTileScene extends Phaser.Scene {
   private graphics: Phaser.GameObjects.Graphics;
-  private hexSize: number = 100;
+  private hexSize: number = 25;
 
   constructor() {
     super({ key: 'hexTileScene' });
@@ -10,16 +10,30 @@ export default class HexTileScene extends Phaser.Scene {
   }
 
   create(): void {
+
+    // Sandbox work
     this.graphics = this.add.graphics();
     this.graphics.lineStyle(3, 0xffffff, 1);
 
     //center point
     let x = 400;
     let y = 100;
-    this.createTile(x, y, this.hexSize);
+    this.createTile(x, y);
+    this.createTileColumn(800,200,7);
   }
-  
-  createTile(x: number, y: number, tileSize: number) {
+
+  getHexHeight() : number{
+      return this.hexSize * Math.sqrt(3);
+  }
+  getHexWidth() : number{
+      return this.hexSize * 2;
+  }
+
+  createTile(x: number, y: number) {
+      // takes XY coordinates of center point,
+      // generates all required vertices
+      // draws individual tile
+
       let points: number[] = this.getHexPointsFromCenter(x, y);
 
       this.graphics.beginPath();
@@ -31,12 +45,26 @@ export default class HexTileScene extends Phaser.Scene {
       this.graphics.strokePath();
     }
 
+    createTileColumn(x:number, y:number, numTiles : number){
+        // creates a column starting from the center point of the first hex,
+        // spans vertically down by numTiles hexes.
+
+        // Uses XY coordinates to find center and draw tile
+        let h = this.getHexHeight();
+
+        for (let i = 0; i < numTiles; i++) {
+            let tile_x = x;
+            let tile_y = y + (i * h);
+            this.createTile(tile_x, tile_y);
+        }
+    }
+
     getHexPointsFromCenter(xPoint: number, yPoint: number): number[] {
       let hexPoints: number[] = [];
-  
+
       // iterate through each corner
       for (let i = 0; i < 6; i++) {
-  
+
         // put each x,y pair in
         this.getHexCorner(xPoint, yPoint, i).forEach(function (val) {
           let length = hexPoints.push(Number(val));
@@ -44,7 +72,7 @@ export default class HexTileScene extends Phaser.Scene {
       }
       return hexPoints;
     }
-  
+
     private getHexCorner(xCenter: number, yCenter: number, angle: number): number[] {
       let angle_deg: number = 60 * angle;
       let angle_rad: number = Math.PI / 180 * angle_deg;
