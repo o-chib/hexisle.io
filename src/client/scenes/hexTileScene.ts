7f3d65e1ed58aa0694eb8b4dae9f62e2f83d0e01
Ajay925 <56622841+Ajay925@users.noreply.github.com>
2021-02-09 23:15:30 -0800

    added helper classes, drawing and coordinates

--- src/client/scenes/hexTileScene.ts
+++ src/client/scenes/hexTileScene.ts
@@ -30,34 +30,69 @@ export default class HexTileScene extends Phaser.Scene {
     this.generateCartMapCoords();
 
     // draw all our hexes
-
+    this.drawAllHexes();
 
     //center point
-    let centerPoint = new Point(MAP_DIAMETER / 2, MAP_DIAMETER / 2);
-    this.createTileColumn(centerPoint, this.getMapHexRadius());
+    //let centerPoint = new Point(MAP_DIAMETER / 2, MAP_DIAMETER / 2);
+    //this.createTileColumn(centerPoint, this.getMapHexRadius());
   }
 
   generateHexMapCoords(): void {
+    // generates the hex integer coordinates from the game-size
+    
+    this.hexMapCoords = [];
     let hexRadius: number = this.getMapHexRadius();
 
     // for each column
-    for (let i = 0; i < (2 * hexRadius) + 1; i++) {
-
+    for (let col = 0; col < (2 * hexRadius) + 1; col++) {
+      this.hexMapCoords[col] = [];
+      
       // for each row
-      for (let j = 0; j < (2 * hexRadius) + 1; j++) {
-        this.hexMapCoords[i][j] = new Point(i, j);
+      for (let row = 0; row < (2 * hexRadius) + 1; row++) {
+        this.hexMapCoords[col][row] = new Point(col, row);
       }
     }
   }
 
   generateCartMapCoords(): void {
+    // generates cartesian coordinates for hex center points based on our hex coordinates
+
+    this.cartMapCoords = [];
+
     // for each column
-    for (let i = 0; i < ; i++) {
+    for (let col = 0; col < this.hexMapCoords.length; col++) {
+      this.cartMapCoords[col] = [];
 
       // for each row
-      for (let j = 0; ; j++) {
+      for (let row = 0; row < this.hexMapCoords[col].length; row++) {
 
-        this.hexMapCoords[i][j] = new Point(i, j);
+        // matrix multiplication for conversions
+        let x: number = this.hexSize * (1.5 * col)
+        //let y: number = this.hexSize * (Math.sqrt(3)/2 * col + Math.sqrt(3) * row);
+        // if the column number is even shift the hexes down, if it's odd don't
+        let y: number;
+        if (col % 2 == 0) {
+          y = this.hexSize * (Math.sqrt(3)/2 + Math.sqrt(3) * row);
+        } else {
+          y = this.hexSize * (Math.sqrt(3) * row);
+        }
+        //this.graphics.scene.add.text(100 + 100 * row, 30 * col, String([Math.round(x * 100) / 100, Math.round(y * 100) / 100]));
+        this.cartMapCoords[col][row] = new Point(x, y);
+      }
+    }
+  }
+
+  drawAllHexes(): void {
+    // draws every hex we have in our map
+    
+    // for each column
+    for (let col = 0; col < this.hexMapCoords.length; col++) {
+      //this.graphics.scene.add.text(100, 30 * col, String(col));
+      
+      // for each row
+      for (let row = 0; row < this.hexMapCoords[col].length; row++) {
+        //this.graphics.scene.add.text(150 + 30 * row, 30 * col, String(row));
+        this.createTile(this.cartMapCoords[col][row])
       }
     }
   }
@@ -102,25 +137,40 @@ export default class HexTileScene extends Phaser.Scene {
     return hexPoints;
   }
 
-  private getHexHeight(): number {
+  getHexHeight(): number {
     return this.hexSize * Math.sqrt(3);
   }
 
-  private getHexWidth(): number {
+  getHexWidth(): number {
     return this.hexSize * 2;
   }
 
-  private getHexCorner(point: Point, angle: number): Point {
+  getHexCorner(point: Point, angle: number): Point {
     let angle_deg: number = 60 * angle;
     let angle_rad: number = Math.PI / 180 * angle_deg;
     return new Point(point.x + this.hexSize * Math.cos(angle_rad),
                      point.y + this.hexSize * Math.sin(angle_rad))
   }
 
-  private getMapHexRadius(): number {
+  getMapHexRadius(): number {
     let mapRadius: number = MAP_DIAMETER / 2;
     let freeSpace: number = mapRadius - (this.getHexHeight() / 2)
     return Math.floor(freeSpace / this.getHexHeight());
+  }
+}
+
+export class Tile {
+  public axial_coord: AxialPoint;
+  public cartesian_coord: Point;
+}
+
+export class AxialPoint {
+  public q: number;
+  public r: number;
+
+  constructor(q = 0, r = 0) {
+    this.q = q;
+    this.r = r;
   }
 }
 
