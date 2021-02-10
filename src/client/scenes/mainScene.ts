bb3a09b351b2fff50a448b03b75f43df6fa54d70
Alex Dunn <adunn@ucdavis.edu>
2021-02-10 00:16:44 -0800

    camera now tracks the player

--- src/client/scenes/mainScene.ts
+++ src/client/scenes/mainScene.ts
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 const Constant = require('./../../shared/constants');
 
 export default class MainScene extends Phaser.Scene {	
-	private mySprite: Phaser.GameObjects.Sprite;
+	private myPlayerSprite: Phaser.GameObjects.Sprite;
 	socket: SocketIOClient.Socket;
 
 	constructor() {
@@ -13,16 +13,20 @@ export default class MainScene extends Phaser.Scene {
 
 	preload(): void {
 		this.load.image('aliem', '../assets/Alien-thumb.jpg');
+		this.load.image('rainbow', '../assets/rainbow.bmp');
 	}
 
 	create(): void {
 		this.socket = io();
 		this.socket.emit(Constant.MESSAGE.JOIN);
 		this.socket.on(Constant.MESSAGE.GAME_UPDATE, this.updateState.bind(this));
+		this.add.sprite(200, 200, 'rainbow');
+		this.myPlayerSprite = this.add.sprite(0, 0, 'aliem');
+		this.myPlayerSprite.setVisible(false);
+		this.cameras.main.startFollow(this.myPlayerSprite, true);
   	}
 
 	updateState(update: any): void { //TODO may state type
-		console.log(update);
 		const { time, currentPlayer, otherPlayers } = update;
 		if (!currentPlayer) {
 			return;
@@ -30,7 +34,9 @@ export default class MainScene extends Phaser.Scene {
 		
 		// Draw background
 		// Draw all players
-		this.mySprite = this.add.sprite(currentPlayer.xPos, currentPlayer.yPos, 'aliem');
+		this.myPlayerSprite.setPosition(currentPlayer.xPos, currentPlayer.yPos);
+		if (!this.myPlayerSprite.visible)
+			this.myPlayerSprite.setVisible(true);
 		otherPlayers.forEach( opp => {
 			this.add.sprite(opp.xPos, opp.yPos, 'aliem');
 		});
