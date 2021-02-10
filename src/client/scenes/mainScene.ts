87483ba615c71877f4554ed63053d00bbdf037b3
Alex Dunn <adunn@ucdavis.edu>
2021-02-10 01:59:08 -0800

    other players no longer duplicate

--- src/client/scenes/mainScene.ts
+++ src/client/scenes/mainScene.ts
@@ -5,6 +5,7 @@ const Constant = require('./../../shared/constants');
 
 export default class MainScene extends Phaser.Scene {	
 	private myPlayerSprite: Phaser.GameObjects.Sprite;
+	private otherPlayerSprites: Map<string, Phaser.GameObjects.Sprite>;
 	socket: SocketIOClient.Socket;
 
 	constructor() {
@@ -18,6 +19,7 @@ export default class MainScene extends Phaser.Scene {
 	}
 
 	create(): void {
+		this.otherPlayerSprites = new Map(); 
 		this.socket = io();
 		this.socket.emit(Constant.MESSAGE.JOIN);
 		this.socket.on(Constant.MESSAGE.GAME_UPDATE, this.updateState.bind(this));
@@ -28,7 +30,7 @@ export default class MainScene extends Phaser.Scene {
 		this.cameras.main.startFollow(this.myPlayerSprite, true);
 
 		this.input.keyboard.on('keydown', (event) => {
-			let direction: number;
+			let direction: number; //TODO make movement smoother
 			switch(event.key) {
 				case "w": direction = Math.PI / 2; break;
 				case "d": direction = 0; break;   // Up, Angle = -90??
@@ -52,8 +54,19 @@ export default class MainScene extends Phaser.Scene {
 		this.myPlayerSprite.setPosition(currentPlayer.xPos, currentPlayer.yPos);
 		if (!this.myPlayerSprite.visible)
 			this.myPlayerSprite.setVisible(true);
+
+		
 		otherPlayers.forEach( opp => {
-			this.add.sprite(opp.xPos, opp.yPos, 'aliem');
+			if (this.otherPlayerSprites.has(opp.socketID)) {
+				this.otherPlayerSprites.get(opp.socketID)!.setPosition(opp.xPos, opp.yPos);
+			} else {
+				let newPlayer = this.add.sprite(opp.xPos, opp.yPos, 'aliem');
+				this.otherPlayerSprites.set(opp.socketID, newPlayer);
+			}
+			//TODO memory leak where old sprites dont get removed
 		});
 	}
 }
+
+
+
