ad01ea6e60e6ab2fa4c93de6b687b6d411213c67
Ajay925 <56622841+Ajay925@users.noreply.github.com>
2021-02-10 21:28:35 -0800

    created error checking and point to hex functions

--- src/client/scenes/hexTileScene.ts
+++ src/client/scenes/hexTileScene.ts
@@ -2,10 +2,10 @@ import { MAP_DIAMETER } from '../config';
 
 export default class HexTileScene extends Phaser.Scene {
   private graphics: Phaser.GameObjects.Graphics;
-  private hexSize: number = 25;
-  private hexMapCoords: Point[][];
-  private cartMapCoords: Point[][];
   private tileMap: Tile[][];
+  private hexRadius: number;
+  private hexSize: number = 25;
+  private campRadius: number = 3;
 
   constructor() {
     super({ key: 'hexTileScene' });
@@ -16,37 +16,59 @@ export default class HexTileScene extends Phaser.Scene {
 
   create(): void {
 
+    this.hexRadius = this.getMapHexRadius();
+
     // Sandbox work
     this.graphics = this.add.graphics();
-    this.graphics.lineStyle(3, 0xffffff, 1);
 
-    this.graphics.scene.add.text(10, 10, String(this.getMapHexRadius()));
-    this.graphics.scene.add.text(10, 30, String(MAP_DIAMETER));
+    this.graphics.scene.add.text(810, 10, String(['hex radius', this.hexRadius]));
+    this.graphics.scene.add.text(810, 30, String(['map diameter', MAP_DIAMETER]));
 
     // generate the tile map
     this.generateTileMap();
 
+    // generate the camps
+    this.generateCamps();
+
     // draw all our hexes in the tile map
     this.drawHexes(this.tileMap);
+
+  }
+
+  update(): void {
+    if (this.input.mousePointer.isDown) {
+      console.log('C L I C K');
+      let testTile: Tile = new Tile('camp');
+      let mouseX: number = this.input.mousePointer.x;
+      let mouseY: number = this.input.mousePointer.y;
+      testTile.axial_coord = this.cartesianToAxial(new Point(mouseX, mouseY));
+      if (this.checkIfValidHex(testTile.axial_coord)) {
+        testTile.cartesian_coord = this.tileMap[testTile.axial_coord.q][testTile.axial_coord.r].cartesian_coord;
+        this.createTile(testTile);
+      }
+    }
   }
 
   generateTileMap(): void {
     // generates the hex integer coordinates from the game-size
     
     this.tileMap = [];
-    let hexRadius: number = this.getMapHexRadius();
 
     // for each column
-    for (let col = 0; col < (2 * hexRadius) + 1; col++) {
+    for (let col = 0; col < (2 * this.hexRadius) + 1; col++) {
       this.tileMap[col] = [];
       
       // for each row
-      for (let row = 0; row < (2 * hexRadius) + 1; row++) {
+      for (let row = 0; row < (2 * this.hexRadius) + 1; row++) {
         this.tileMap[col][row] = new Tile();
         this.tileMap[col][row].axial_coord = new AxialPoint(col, row);
         this.tileMap[col][row].cartesian_coord = this.axialToCartesian(this.tileMap[col][row].axial_coord);
       }
     }
+  }
+
+  generateCamps(): void {
+
   }
 
   drawHexes(tileMap: Tile[][]): void {
@@ -57,17 +79,23 @@ export default class HexTileScene extends Phaser.Scene {
       
       // for each row
       for (let row = 0; row < tileMap[col].length; row++) {
-        this.createTile(tileMap[col][row].cartesian_coord)
+        this.createTile(tileMap[col][row])
       }
     }
   }
 
-  createTile(point: Point) {
+  createTile(tile: Tile): void {
     // takes XY coordinates of center point,
     // generates all required vertices
     // draws individual tile
 
-    let points: Point[] = this.getHexPointsFromCenter(point);
+    let points: Point[] = this.getHexPointsFromCenter(tile.cartesian_coord);
+
+    if (tile.building == 'camp') {
+      this.graphics.lineStyle(4, 0xff0000, 1);
+    } else {
+      this.graphics.lineStyle(2, 0xffffff, 1);
+    }
 
     this.graphics.beginPath();
     this.graphics.moveTo(points[0].x, points[0].y);
@@ -78,7 +106,17 @@ export default class HexTileScene extends Phaser.Scene {
     this.graphics.strokePath();
   }
 
-  createTileColumn(point: Point, numTiles: number){
+  checkIfValidHex(axialPoint: AxialPoint): boolean {
+    // takes in an axial coordinate and returns if that hex exists
+
+    if (axialPoint.q < 0 || axialPoint.r < 0 ||
+        axialPoint.q > this.hexRadius * 2 || axialPoint.r > this.hexRadius * 2) {
+          return false;
+        }
+    return true;
+  }
+
+  createTileColumn(point: Point, numTiles: number): void {
       // creates a column starting from the center point of the first hex,
       // spans vertically down by numTiles hexes.
 
@@ -88,11 +126,13 @@ export default class HexTileScene extends Phaser.Scene {
       for (let i = 0; i < numTiles; i++) {
           let tile_x = point.x;
           let tile_y = point.y + (i * h);
-          this.createTile(new Point(tile_x, tile_y));
+          let tile: Tile = new Tile();
+          tile.axial_coord = new AxialPoint(tile_x, tile_y);
+          this.createTile(tile);
       }
   }
 
-  getHexPointsFromCenter(point: Point): Point[] {
+  private getHexPointsFromCenter(point: Point): Point[] {
     let hexPoints: Point[] = [];
 
     // iterate through each corner
@@ -102,46 +142,112 @@ export default class HexTileScene extends Phaser.Scene {
     return hexPoints;
   }
 
-  getHexHeight(): number {
+  private getHexHeight(): number {
     return this.hexSize * Math.sqrt(3);
   }
 
-  getHexWidth(): number {
+  private getHexWidth(): number {
     return this.hexSize * 2;
   }
 
-  getHexCorner(point: Point, angle: number): Point {
+  private getHexCorner(point: Point, angle: number): Point {
     let angle_deg: number = 60 * angle;
     let angle_rad: number = Math.PI / 180 * angle_deg;
     return new Point(point.x + this.hexSize * Math.cos(angle_rad),
                      point.y + this.hexSize * Math.sin(angle_rad))
   }
 
-  getMapHexRadius(): number {
+  private getMapHexRadius(): number {
     let mapRadius: number = MAP_DIAMETER / 2;
     let freeSpace: number = mapRadius - (this.getHexHeight() / 2)
     return Math.floor(freeSpace / this.getHexHeight());
   }
 
-  axialToCartesian(axialPoint: AxialPoint): Point {
+  private axialToCartesian(axialPoint: AxialPoint): Point {
     
     // matrix multiplication for conversions
-    let x: number = (this.hexSize * (1.5 * axialPoint.q)) + this.hexSize
+    let x: number = (this.hexSize * (1.5 * axialPoint.q));
 
-    // if the column number is even shift the hexes down, if it's odd don't
+    // if the column number is odd shift the hexes down, if it's even don't
     let y: number;
-    if (axialPoint.q % 2 == 0) {
+    if (axialPoint.q % 2 != 0) {
       y = this.hexSize * (Math.sqrt(3)/2 + Math.sqrt(3) * axialPoint.r);
     } else {
       y = this.hexSize * (Math.sqrt(3) * axialPoint.r);
     }
-    return new Point(x, y);
+    return new Point(x + this.hexSize, y + this.hexSize);
+  }
+
+  private cartesianToAxial(point: Point): AxialPoint {
+    // takes in a point in the cartesian plane
+    // returns the coordinate of the hex it's in
+    
+    // account for the half a hex to the bottom right we're pushing the map
+    point.x = point.x - (this.hexSize);
+    point.y = point.y - (this.hexSize);
+
+    // convert the pixel location to an axial location
+    let q: number = (2/3 * point.x) / this.hexSize;
+    let r: number = (-1/3 * point.x + Math.sqrt(3)/3 * point.y) / this.hexSize;
+
+    // make these axial coords into a cube format
+    let cube: number[] = this.axialToCube(new AxialPoint(q, r));
+
+    // round the cube
+    cube = this.roundCube(cube);
+
+    // convert the cube to evenq coords and return
+    console.log(this.cubeToOddq(cube).q, this.cubeToOddq(cube).r)
+    return this.cubeToOddq(cube);
+  }
+
+  private axialToCube(axialPoint: AxialPoint): number[] {
+    // used for conversions from cartesian to axial
+    // returns x, y, z in that order in a list
+  
+    let x: number = axialPoint.q;
+    let z: number = axialPoint.r;
+    let y: number = -x - z;
+    return [x, y, z];
+  }
+
+  private cubeToOddq(cube: number[]): AxialPoint {
+    // used for conversions from cartesian to axial
+    // returns an axial point
+  
+    let col: number = cube[0];
+    let row: number = cube[2] + (cube[0] - (cube[0]&1)) / 2;
+    return new AxialPoint(col, row);
+  }
+
+  private roundCube(cube: number[]): number[] {
+    let rx: number = Math.round(cube[0]);
+    let ry: number = Math.round(cube[1]);
+    let rz: number = Math.round(cube[2]);
+
+    var x_dif = Math.abs(rx - cube[0]);
+    var y_dif = Math.abs(ry - cube[1]);
+    var z_dif = Math.abs(rz - cube[2]);
+
+    if (x_dif > y_dif && x_dif > z_dif) {
+        rx = -ry - rz;
+    } else if (y_dif > z_dif) {
+        ry = -rx - rz;
+    } else {
+        rz = -rx - ry;
+    }
+    return [rx, ry, rz];
   }
 }
 
 export class Tile {
   public axial_coord: AxialPoint;
   public cartesian_coord: Point;
+  public building: string;
+
+  constructor(building: string = 'none') {
+    this.building = building;
+  }
 }
 
 export class AxialPoint {
