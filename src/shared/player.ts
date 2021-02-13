44e4cf6393a1a0ed72f23868ba3758b20b8b9e06
Alex Dunn <adunn@ucdavis.edu>
2021-02-13 03:40:09 -0800

    moves socket into player class and removes the map for it

--- src/shared/player.ts
+++ src/shared/player.ts
@@ -5,15 +5,15 @@ export default class Player { // extends Phaser.Physics.Matter.Sprite
 	xVel: number;
 	yVel: number;
 	color: string = "0x"+Math.floor(Math.random()*16777215).toString(16);
-	direction: number;
+	socket: SocketIOClient.Socket;
 
-	constructor(id: string, xPos: number, yPos: number) {
-		this.id = id;
+	constructor(socket: SocketIOClient.Socket, xPos: number, yPos: number) {
+		this.id = socket.id;
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.xVel = 0;
 		this.yVel = 0;
-		this.direction = 0;
+		this.socket = socket;
 	}
 
 	serializeForUpdate() {
