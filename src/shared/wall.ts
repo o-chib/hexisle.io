b65eec9005d6bbce3a9e0b8ef8a82f878ec75c2a
Alex Dunn <adunn@ucdavis.edu>
2021-02-25 19:55:25 -0800

    create a wall object

---
+++ src/shared/wall.ts
@@ -0,0 +1,33 @@
+import { Tile } from "./hexTiles";
+
+export default class Wall {
+	id: string;
+	xPos: number;
+	yPos: number;
+	expirationDate: number;
+	teamNumber: number;
+	tile: Tile;
+
+	hp = 10;
+
+	constructor(id: string, xPos: number, yPos: number, teamNumber: number) {
+		this.id = id;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.teamNumber = teamNumber;
+	}
+
+	serializeForUpdate(): any {
+		return {
+			id: this.id,
+			xPos: this.xPos,
+			yPos: this.yPos,
+			hp: this.hp,
+			team: this.teamNumber,
+		};
+	}
+
+	isAlive(): boolean {
+		return this.hp > 0;
+	}
+}
