a10bbf8e49ca697c4c01b755cdb293c1622a7c68
Alex Dunn <adunn@ucdavis.edu>
2021-04-08 01:56:42 -0700

    formatting

--- tests/easyTest.js
+++ tests/easyTest.js
@@ -1,6 +1,7 @@
-describe('getDivBg', function() {
-    it('This should be trivial', function() {
-		let a = 5, b = 3 + 2;
-        expect(a).toEqual(b);
-    });
+describe('getDivBg', function () {
+	it('This should be trivial', function () {
+		let a = 5,
+			b = 3 + 2;
+		expect(a).toEqual(b);
+	});
 });
