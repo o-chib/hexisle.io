262f330753f7bce7ad793f27bffbf35b5995479b
Alex Dunn <adunn@ucdavis.edu>
2021-04-10 04:20:35 -0700

    update test a bit

--- tests/bullet.test.ts
+++ tests/bullet.test.ts
@@ -2,15 +2,15 @@ import Bullet from '../src/shared/bullet';
 const Constant = require('../src/shared/constants');
 
 describe('Bullet', () => {
-	it('Bullet should move at right speed', () => {
+	it('should move at right speed', () => {
 		const x = 1;
 		const y = 1;
 		const bullet = new Bullet('0', x, y, Constant.DIRECTION.N, 0);
 
 		expect(bullet.yPos).toEqual(y);
 
-		bullet.updatePosition(10);
+		bullet.updatePosition(100);
 
-		expect(bullet.yPos).toEqual(y + bullet.speed * 10);
+		expect(bullet.yPos).toEqual(y + bullet.speed * 100);
 	});
 });
