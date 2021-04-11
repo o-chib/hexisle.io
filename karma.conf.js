28eec05c0ae3f4e831e4cf57a3a7a5e195e8f99d
Alex Dunn <adunn@ucdavis.edu>
2021-04-11 04:07:31 -0700

    cleanup formatting

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '.',
+    basePath: '',
 
 
     // frameworks to use
@@ -40,9 +40,9 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
+      'progress',
       'karma-typescript',
-      'progress'
-  ],
+    ],
 
 
     // web server port
