5184b63ec4021ce4ef80b54acfdd9210a6aa8c6c
Alex Dunn <adunn@ucdavis.edu>
2021-04-11 17:12:24 -0700

    exclude server file

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'tests/**/*.ts',
-      'src/**/*.ts',
+      'src/**/!(server).ts',
       {pattern: 'public/assets/*', watched: false, included: false, served: true},
     ],
 
@@ -75,6 +75,7 @@ module.exports = function(config) {
     concurrency: Infinity,
 
     karmaTypescriptConfig: {
+      compilerOptions: {},
       tsconfig: "./tsconfig.eslint.json",
     }
   })
