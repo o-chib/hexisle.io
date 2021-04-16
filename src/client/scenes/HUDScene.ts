018445c5c50901fb136bc6e445e4295329c36a00
Alex Dunn <adunn@ucdavis.edu>
2021-04-16 14:55:07 -0700

    add timer

--- src/client/scenes/HUDScene.ts
+++ src/client/scenes/HUDScene.ts
@@ -4,12 +4,15 @@ import Anchor from 'phaser3-rex-plugins/plugins/anchor.js';
 const info_format = `Health:	%1
 Score:	%2
 Resource:	%3`;
+const timer_format = `%1:%2`;
 
 export default class HUDScene extends Phaser.Scene {
 	private mainSceneObj: any;
 
 	// Text/Scoring
 	private infoText?: Phaser.GameObjects.Text;
+	// Game timer
+	private gameTimeText?: Phaser.GameObjects.Text;
 
 	constructor() {
 		super({ key: 'HUDScene', active: true });
@@ -22,6 +25,15 @@ export default class HUDScene extends Phaser.Scene {
 			top: 'top+10',
 		});
 
+		this.gameTimeText = this.add.text(0, 0, '', {
+			font: '48px Arial',
+			align: 'center',
+		});
+		new Anchor(this.gameTimeText, {
+			centerX: 'center',
+			top: 'top+10',
+		});
+
 		//  Grab a reference to the Game Scene
 		this.mainSceneObj = this.scene.get('MainScene');
 
@@ -29,12 +41,30 @@ export default class HUDScene extends Phaser.Scene {
 		this.mainSceneObj.events.on('updateHUD', this.updateText, this);
 	}
 
-	private updateText(currentPlayer: any): void {
-		const text = Phaser.Utils.String.Format(info_format, [
+	private updateText(currentPlayer: any, time: number): void {
+		const playerText = Phaser.Utils.String.Format(info_format, [
 			currentPlayer.health,
 			currentPlayer.score,
 			currentPlayer.resources,
 		]);
-		this.infoText?.setText(text);
+		this.infoText?.setText(playerText);
+
+		if (time < 0) time = 0;
+		const dateTime = new Date(time);
+		const minutes = dateTime.getMinutes();
+		const seconds = dateTime.getSeconds();
+		//console.log(dateTime.format("mm:ss"));
+
+		const gameTimeText = Phaser.Utils.String.Format(timer_format, [
+			this.addLeadingZeros(minutes),
+			this.addLeadingZeros(seconds),
+		]);
+		this.gameTimeText?.setText(gameTimeText);
+	}
+
+	private addLeadingZeros(time: number): string {
+		const timeStr = time.toString();
+		if (time > 9) return timeStr;
+		return '0' + timeStr;
 	}
 }
