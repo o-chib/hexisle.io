3637e36371f08debd53bc4ca7ca7952d5fca15e9
Karan Gupta <32601454+kargupta28@users.noreply.github.com>
2021-04-19 04:05:55 -0700

    Generating and validating a random point on the grid.

--- src/shared/hexTiles.ts
+++ src/shared/hexTiles.ts
@@ -213,6 +213,20 @@ export class HexTiles {
 		return true;
 	}
 
+	checkIfValidPointOnGrid(point: Point): boolean {
+		let hexPoint: OffsetPoint = this.cartesianToOffset(point);
+
+		if(!this.checkIfValidHex(hexPoint)) return false;
+
+		if(this.tileMap[hexPoint.q][hexPoint.r].building != Constant.BUILDING.NONE) return false;
+
+		return true;
+	}
+
+	getRandomMapPoint(): Point {
+        return new Point(Math.random() * this.mapHeight, Math.random() * this.mapHeight);
+    }
+
 	getHexRingPoints(centreTile: Tile, radius: number): OffsetPoint[] {
 		// Takes center tile and returns a list of points that form a ring at some radius from the center
 		// radius = number of tiles from top to center - excluding the center.
