218f0269a7986f17a0ffd994577574320fce56d7
Ajay925 <56622841+Ajay925@users.noreply.github.com>
2021-04-20 20:49:11 -0700

    turret class

---
+++ src/shared/turret.ts
@@ -0,0 +1,43 @@
+import { Constant } from './constants';
+import { Tile } from './hexTiles';
+
+export default class Turret {
+	id: string;
+	xPos: number;
+	yPos: number;
+	direction: number;
+	teamNumber: number;
+	tile: Tile;
+	hp: number;
+
+	constructor(
+		id: string,
+		xPos: number,
+		yPos: number,
+		teamNumber: number,
+		tile: Tile
+	) {
+		this.id = id;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.direction = 0;
+		this.teamNumber = teamNumber;
+		this.tile = tile;
+		this.hp = Constant.HP.TURRET;
+	}
+
+	serializeForUpdate(): any {
+		return {
+			id: this.id,
+			xPos: this.xPos,
+			yPos: this.yPos,
+			direction: this.direction,
+			hp: this.hp,
+			teamNumber: this.teamNumber,
+		};
+	}
+
+	isAlive(): boolean {
+		return this.hp > 0;
+	}
+}
