318e0b51cf45233fb71892c8cc71168f88c75c03
Ajay925 <56622841+Ajay925@users.noreply.github.com>
2021-04-30 20:13:01 -0700

    fixing names

---
+++ tests/mapResources.test.ts
@@ -0,0 +1,27 @@
+import { Point } from '../src/shared/hexTiles';
+import { MapResources, Resource } from '../src/server/mapResources';
+import { Constant } from '../src/shared/constants';
+
+describe('Resource', () => {
+	it('should generate correct resourceCount', () => {
+		const mapResources = new MapResources();
+		const xPos = 1;
+		const yPos = 1;
+		const point = new Point(xPos, yPos);
+		const numResourceToGenerate =
+			mapResources.getRandomResourceGenerationCount() +
+			mapResources.minResource;
+
+		let i = 0;
+		while (numResourceToGenerate > i) {
+			mapResources.generateResource(i.toString(), point);
+			i++;
+		}
+		expect(mapResources.resourceCount).toEqual(numResourceToGenerate);
+
+		const newResource = new Resource(i.toString(), xPos, yPos, 2, 'BLACK');
+		mapResources.resources.add(newResource);
+
+		expect(mapResources.resources.has(newResource)).toEqual(true);
+	});
+});
