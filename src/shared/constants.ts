abd5206b361bd77bc53372c6e3127815a4e9ed72
Alex Dunn <adunn@ucdavis.edu>
2021-05-02 02:03:59 -0700

    update game timer

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -115,8 +115,7 @@ export const Constant = Object.freeze({
 
 	TIMING: {
 		SERVER_GAME_UPDATE: 1000 / 60,
-		CHECK_GAME_END: 1000 / 60,
 		GAME_END_SCREEN: 5 * 1000,
-		GAME_TIME_LIMIT: 5 * (60 * 1000),
+		GAME_TIME_LIMIT: 15 * (60 * 1000),
 	},
 });
