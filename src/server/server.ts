46e07554bb5b02b0356cf2bc631546fdcf881eac
Oorjit Chib <ochib@ucdavis.edu>
2021-05-24 17:09:00 -0700

    Bad name doesn't get assigned + empty name.

--- src/server/server.ts
+++ src/server/server.ts
@@ -35,13 +35,14 @@ allGames.newGame();
 const websocket = new SocketIO.Server(server);
 websocket.on('connection', function (socket: SocketIO.Socket) {
 	socket.on(Constant.MESSAGE.JOIN_GAME, (name?: string, gameID?: string) => {
-		if (!name) name = 'Aliem';
+		if (!name) name = '';
 
 		if (filter.isProfane(name)) {
 			socket.emit(
 				Constant.MESSAGE.JOIN_GAME_FAIL,
 				'Please use a different name'
 			);
+			return;
 		}
 
 		if (allGames.addPlayerToGame(socket, name, gameID))
